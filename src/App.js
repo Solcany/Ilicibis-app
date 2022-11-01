0905028db1cf16ea212d68b32bbf1b7a5329bfb9
m <matus.solcany@protonmail.com>
2022-11-01 13:37:20 +0100

    more components

--- src/App.js
+++ src/App.js
@@ -1,18 +1,32 @@
-import logo from 'logo.svg';
+import {useState} from 'react';
+import {AppProvider} from "Contexts/AppContext.js"
+import ModalCard from "Components/ModalCard"
+import Button from "Components/Button";
 import Header from "Components/Header"
 import RandomVideoPlayer from "Components/RandomVideoPlayer"
 import {VIDEO_ERROR_UI_MESSAGES,UNKNOWN_ERROR_UI_MESSAGE} from "Constants/constants";
 import 'Stylesheets/Reset.scss';
 import 'Stylesheets/App.scss';
 
-function App() {
+const App = () => {
+  const [isModalVisible, setIsModalVisible] = useState(true)
+
+  const handleMuteButtonClick = () => {
+    setIsModalVisible(false)
+  }
+
   return (
-    <div className="app">
-      <Header className="header"/>
-      <section className="ilicibis">
-        <RandomVideoPlayer/>
-      </section>
-    </div>
+    <AppProvider>
+      <div className="app">
+        <ModalCard isVisible={isModalVisible}>
+          <Button onClick={handleMuteButtonClick}> Watch </Button>
+        </ModalCard>
+        <Header className="header"/>
+        <section className="ilicibis">
+          <RandomVideoPlayer/>
+        </section>
+      </div>
+    </AppProvider>
   );
 }
 
---
+++ src/Components/Button.js
@@ -0,0 +1,17 @@
+import "Stylesheets/Button.scss";
+import {joinStyles} from "Utils/dev.js"
+
+const Button = ({children, 
+				 onClick, 
+				 className}) => {
+
+	return (
+		<button onClick={onClick} 
+				className={joinStyles(["button", className])}>
+				{children}
+		</button>
+	)
+
+}
+
+export default Button
